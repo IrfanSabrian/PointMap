b3ce82aa792e25396309998529281bec0729d20c
IrfanSabrian <irfansabrian34@gmail.com>
2025-08-08 18:40:56 +0700

    fix: update default theme setting in ThemeProvider from 'system' to 'light' for improved user experience

--- frontend/src/app/theme-provider.tsx
+++ frontend/src/app/theme-provider.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function ThemeRegistry({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       {children}
     </ThemeProvider>
   );
